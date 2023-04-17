8386c3c57dadf070e80de83c278693bc649dc2f5
Juanchi <juanchocesarotti!@gmail.com>
2023-04-17 17:26:56 -0300

    New Commit con Modelo

---
+++ Users_Crud/Controllers/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Users.Controllers
+{
+    public class Users_Crud : Controllers
+    {
+        public Users_Crud()
+        {
+            private DB_UsersCrudEntity db = new DB_UsersCrudEntity();
+            
+            public actionResult Index ()
+            {
+               return view .Index (db user);
+            }
+            
+        }
+    }
+}
---
+++ Users_Crud/Models/Users_Crud.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Users_Crud.Models
+{
+    using System;
+
+
+
+    public class Users_Crud
+    {
+        public partial class Users_Crud
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }    
+            public string Email { get; set; }
+            public string Password { get; set; }    
+            public string Username { get; set; } = null;
+            public string UserEmail { get; set; }   
+            public string PasswordEmail { get; set; }   
+                    
+        }
+    }
+
+}
+
+
